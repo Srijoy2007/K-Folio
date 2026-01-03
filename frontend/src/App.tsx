0b97c58774355cf51971791e63a7702b71ec1090
garimakumarix29 <garimakumari.x29@gmail.com>
2026-01-03 14:36:35 +0530

    routes for landing page and signin component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,24 @@
-import LandingPage from "./pages/Landing/LandingPage"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import LandingPage from "./pages/Landing/LandingPage";
+import "./App.css";
+import SignIn from "./components/SignIn";
 
-import "./App.css"
-import SignIn from "./components/SignIn"
 function App() {
-
   return (
-    <>
-      <LandingPage />
-    <SignIn />
-    </>
-  )
+    <BrowserRouter>
+      <nav style={{ padding: 12 }}>
+        <Link to="/" style={{ marginRight: 12 }}>
+          Home
+        </Link>
+        <Link to="/signin">Sign In</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
