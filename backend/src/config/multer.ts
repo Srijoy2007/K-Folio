d1383cc07a11f6128f6833167be1d9ebed7e0f4f
Dreamscape <dreamscape.dev>
2026-01-09 01:32:19 +0530

    nioaf

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const dir = path.join(__dirname, "../../uploads");
+    const dir = path.join(__dirname, "../../uploadstemp");
     if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
     cb(null, dir);
   },
